5c96a1291ca2854b3335cd01bc1c478d73ead33b
rcy <21010078@mail.ecust.edu.cn>
2023-11-22 15:39:57 +0800

    new

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,64 +1,53 @@
 <template >
+  <h1 style="color: #67C23A;text-align: center;border-bottom:2px solid #67C23A">推荐系统</h1>
   <el-row :gutter="18">
-    <el-col :span="3"><div class="grid-content ep-bg-purple" id="price" />
-      <span>价格</span>
+    <el-col :span="4"></el-col>
+    <el-col :span="6">
+      <p>价格</p>
       <el-input-number
           :precision="2" :step="0.1" :min="0"
           size="large"
       v-model="price"
-      class="w-50 m-2"
       type="text"
       placeholder="￥"
     />
     </el-col>
-    <el-col :span="3"><div class="grid-content ep-bg-purple" id="classid" />
-      <span>类别(70-90)</span>
+    <el-col :span="6">
+      <p>类别(70-90)</p>
       <el-input
       size="large"
       v-model="cid"
-      class="w-50 m-2"
       type="text"
       placeholder="商品类别"
       :prefix-icon="CollectionTag"
     />
     </el-col>
-<!--    <el-col :span="3"><div class="grid-content ep-bg-purple" id="goodname" />-->
-<!--      <span>名称</span>-->
-<!--      <el-input-->
-<!--          label="商品名称"-->
-<!--      size="large"-->
-<!--      v-model="goodname"-->
-<!--      class="w-50 m-2"-->
-<!--      type="text"-->
-<!--      placeholder="商品名称"-->
-<!--      :prefix-icon="Goods"-->
-<!--    />-->
-<!--    </el-col>-->
-    <el-col :span="3"><div class="grid-content ep-bg-purple" />
-      <el-button type="primary" @click="submit" :icon="Search" id="search">搜索</el-button>
+    <el-col :span="4">
+      <p> <br></p>
+      <el-button type="success" @click="submit" :icon="Search" id="search">搜索</el-button>
     </el-col>
-
   </el-row>
-    <div id="content" style="margin-top:120px ;">
-      <!--具体的每个商品-->
-    <el-row :gutter="12" style="flex: auto; flex-direction: row;">
-      <el-col :span="4" v-for="item in content" :key="item.id">
-        <el-card shadow="hover"> 
-          <p>商品名：{{ item.gname }}</p>
-          <p>商品号：{{ item.gnum }}</p>
-          <p>价格：{{ item.gprice }}</p>
-        </el-card>
-      </el-col>
-    </el-row>
 
+    <div id="content" style="margin:30px; border:2px solid #67C23A; border-radius: 10px ;min-height: 36px;">
+      <!--具体的每个商品-->
+      <el-row :gutter="12">
+        <el-col :span="4" v-for="item in content" :key="item.id">
+          <el-card shadow="hover">
+            <p>商品名：{{ item.gname }}</p>
+            <p>商品号：{{ item.gnum }}</p>
+            <p>价格：{{ item.gprice }}</p>
+          </el-card>
+        </el-col>
+      </el-row>
     </div>
 </template>
 <style>
 /* 其他样式 */
-@import "@/assets/styles.css";
+@import "./assets/styles.css";
 </style>
 <script>
 import { Goods,Search,CollectionTag } from '@element-plus/icons-vue';
+import {ElMessage} from "element-plus";
 import axios from 'axios';
 export default {
   data(){
@@ -97,13 +86,14 @@ export default {
         instance.post('/',  jsonData )
           .then(response => {
             console.log('请求成功', response.data);
+            ElMessage.success("请求成功")
             const responseData = response.data;
             const jsonString = JSON.stringify(responseData);
-            const goodlist = JSON.parse(jsonString);
+            const goodList = JSON.parse(jsonString);
 
             for (let i = 0; i < 20; i++) {
             // 获取商品信息
-            const currentGood = goodlist.tui_list[i];
+            const currentGood = goodList.tui_list[i];
 
             // 将商品信息添加到content列表中
             this.content.push({
@@ -117,6 +107,7 @@ export default {
           })
           .catch(error => {
             console.error('请求失败', error);
+            ElMessage.error("连接超时")
           });
 
     
